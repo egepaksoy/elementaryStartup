3c7f6971e82f7199060f76a03874a5fa837fdd7f
Ege Paksoy <egepaskoy@gmail.com>
2021-02-10 20:59:48 +0300

    Birkac ince ayar yaptim

--- startup.py
+++ startup.py
@@ -5,7 +5,7 @@ file = open('setupLog.txt', 'r')
 
 if file.readline() == '\n':
 
-    os.system('sudo apt update && sudo apt upgrade -y')
+    os.system('sudo apt-get update && sudo apt-get upgrade -y')
     file = open('setupLog.txt', 'w')
     file.write('update\nupgrade')
     file.close()
@@ -13,20 +13,41 @@ if file.readline() == '\n':
 
 else:
     print('success')
-    os.system('sudo apt install git && git clone https://github.com/btd1337/La-Sierra-Icon-Theme ~/.icons/La-Sierra')
+
+    os.system('sudo apt-get install software-properties-common')
+    os.system('sudo apt-get install apt-get-transport-https')
+    os.system('sudo apt-get install gedit -y')
+
+
+    os.system('sudo snap install code --classic')
+
+    
+    os.system('sudo apt-get install git')
+    os.system('git clone https://github.com/btd1337/La-Sierra-Icon-Theme ~/.icons/La-Sierra')
     os.system('git clone https://github.com/btd1337/eOS-Sierra-Gtk ~/.themes/eOS-Sierra-Gtk')
     os.system('gsettings set org.gnome.desktop.interface icon-theme "La-Sierra"')
     os.system('gsettings set org.gnome.desktop.interface gtk-theme "eOS-Sierra-Gtk"')
-    os.system('sudo apt install software-properties-common')
-    os.system('sudo snap install code --classic')
+    
+    
+    os.system('sudo apt-get install vim -y')
     os.system('git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime')
     os.system('sh ~/.vim_runtime/install_awesome_vimrc.sh')
-    os.system('wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -')
-    os.system('sudo apt-get install apt-transport-https')
-    os.system('echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list')
-    os.system('sudo apt-get update')
+
+    
+    os.system('wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-get-key add -')
+    os.system('echo "deb https://download.sublimetext.com/ apt-get/stable/" | sudo tee /etc/apt-get/sources.list.d/sublime-text.list')
     os.system('sudo apt-get install sublime-merge') 
-    os.system('sudo apt-get install gedit -y')
+
+
     os.system('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash')
     os.system('nvm install --lts')
+
+
+    os.system('sudo apt-get install python3-pip')
+    os.system('pip3 install virtualenv')
+
+
+    os.system('sudo apt-get update')
+    os.system('sudo apt-get upgrade -y')
+    os.system('echo "indirmen gereken birkac uygulama kaldı bunlar Discord Zoom Telegram ScreenRec Firefox fira code"')
     os.system('rm setupLog.txt')
